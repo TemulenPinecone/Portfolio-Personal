59206713505cc8f4c664288ae258dc50082b6574
Тэмүлэн Галсанпунцаг <23LP2607@G401-CC0119.local>
2024-01-16 14:17:08 +0800

    class

---
+++ src/pages/portfolio.js
@@ -0,0 +1,19 @@
+import Header from "@/components/Header";
+
+export default function Portfolio() {
+  return (
+    <div>
+      <div>
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+// <p>Hero</p> {/*<Hero />*/}
+// <p>About</p> {/*<About />*/}
+// <p>Skills</p> {/*<Skills />*/}
+// <p>Experience</p> {/*<Experience />*/}
+// <p>Work</p> {/*<Work />*/}
+// <p>ContactMe</p> {/*<ContactMe />*/}
+// <p>Footer</p> {/*<Footer />*/}
