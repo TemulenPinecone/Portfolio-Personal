493fb1903c4ef2f8becfb0bc5a780377d5322bdb
Тэмүлэн Галсанпунцаг <23LP2607@G401-CC0119.local>
2024-01-17 13:55:42 +0800

    class

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,10 +3,26 @@ function AboutMe() {
   return (
     <div>
       <div className="container bg-white">
+        {/* ABOUT ME */}
         <div className="flex justify-center">
           <button className="rounded-lg text-gray-600 text-sm px-3 py-1 bg-gray-200">
             About me
           </button>
+        </div>
+
+        <div>
+          <div>
+            <div className="picContainer">
+              <img
+                src="https://picjumbo.com/wp-content/uploads/beautiful-nature-scenery-free-photo-2210x1473.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+          <div>
+            <p>Curious about me? Here you have it:</p>
+            <div></div>
+          </div>
         </div>
       </div>
     </div>
